e92ed5b5b14f3eeae47a147a4004528aab58996b
Derek Araujo <derekaraujo19@gmail.com>
2022-07-19 18:31:26 +0200

    lab complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+attr_reader :anagram
+
+def initialize(anagram)
+  @anagram = anagram
+end
+
+def match(words)
+  # takes an array of possible anagrams
+  # It should return all matches in an array. If no matches exist, it should return an empty array.
+
+  words.select do |word|
+    word.chars.sort == @anagram.chars.sort
+  end
+
+
+end
+
+
+
+end
